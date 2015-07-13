c2820552831f1855d42085396f59032d499b4af6
j-peterson <jtpeterson@mdpnp.org>
2015-07-13 11:42:03 -0400

    testing Mongo DB query for find all

--- analytics/downloads-analytics.js
+++ analytics/downloads-analytics.js
@@ -2,11 +2,30 @@
 var http = require('http');
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
-var argv = require('minimist')(process.argv.slice(2));
+// var argv = require('minimist')(process.argv.slice(2));
+var port = 8888;
+
+MongoClient.connect('mongodb://127.0.0.1:27017/github-downloads', function(err, db) {
+    assert.equal(null, err);
+
+    db.collection('downloads').find({}).toArray(function(err, result) {
+        assert.equal(null, err);
+        // assert.equal(1, result);
+
+        console.log(result);
+
+        db.close();
+    });
+});
+
+// function serverResponse(request, response) {
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.write('Hello, World!');
+//     response.end();
+// }
+
+// http.createServer(serverResponse).listen(port, function() {
+//     console.log('Server listening on: http://localhost:', port);
+// });
 
 
-
-
-// listen for REST gets for downloads data
-// query mongo for downloads data
-// post data

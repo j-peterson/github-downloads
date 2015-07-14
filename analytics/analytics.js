74c992dc0b7c63fa78ad0999e9bc6a93270946a5
j-peterson <jtpeterson@mdpnp.org>
2015-07-14 13:33:03 -0400

    simplifying the architecture ...

---
+++ analytics/analytics.js
@@ -0,0 +1,22 @@
+'use-strict';
+var MongoClient = require('mongodb').MongoClient;
+var fs = require('fs');
+var assert = require('assert');
+var argv = require('minimist')(process.argv.slice(2));
+var port = argv.p || argv.port;
+assert.ok(port > 0 && port < 49151);
+
+MongoClient.connect('mongodb://127.0.0.1:27017/github-downloads', function(err, db) {
+    assert.equal(null, err);
+
+    db.collection('downloads').find({}, function(err, result) {
+        assert.equal(null, err);
+        // assert.ok(result.length);
+
+
+        console.log(typeof result);
+        console.log(result);
+
+        db.close();
+    });
+});

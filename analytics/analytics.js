7680eab357d6d4590305981473799df491b93add
j-peterson <jtpeterson@mdpnp.org>
2015-07-14 13:38:33 -0400

    include mainmast in package.json and remove port info

--- analytics/analytics.js
+++ analytics/analytics.js
@@ -3,8 +3,6 @@ var MongoClient = require('mongodb').MongoClient;
 var fs = require('fs');
 var assert = require('assert');
 var argv = require('minimist')(process.argv.slice(2));
-var port = argv.p || argv.port;
-assert.ok(port > 0 && port < 49151);
 
 MongoClient.connect('mongodb://127.0.0.1:27017/github-downloads', function(err, db) {
     assert.equal(null, err);

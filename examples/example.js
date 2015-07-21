c4320178bf6aa75e6cec4c87d05965b31b539eee
j-peterson <jtpeterson@mdpnp.org>
2015-07-21 00:04:00 -0400

    starting a D3 thing. successfully retrieving and formatting data. need to actually make graph now.

--- examples/example.js
+++ examples/example.js
@@ -1,39 +1,57 @@
 'use strict';
 
-function getDownloadsData (callback) {
-    d3.json('https://www.openice.info/files/github-downloads-data.txt', function (err, data) {
+function getDownloadsData (url, callback) {
+    function parseDownloads (data) {
+        var formattedData = data.map(function (obj) {
+            var newInsert = [];
+            obj.assets.forEach(function (value, index, array) {
+                var newObj = {};
+                newObj.dateRetrieved = obj.dateRetrieved;
+                newObj.series = value.name;
+                newObj.download_count = value.download_count;
+                newInsert.push(newObj);
+            });
+            return newInsert;
+        });
+
+        var flattenedData = formattedData.reduce(function (a, b) {
+            return a.concat(b);
+        });
+
+        callback(flattenedData);
+    }
+
+    d3.json(url, function (err, data) {
         if (err) {
             d3.json('example-data.json', function (err, data) {
                 if (err) throw err;
                 console.log('Using backup data');
-                callback(data);
+                parseDownloads(data);
             });
         } else {
-            callback(data);
+            parseDownloads(data);
         }
     });
 }
 
-function parseDownloads (data) {
-    var formattedData = data.map(function (obj) {
-        var newInsert = [];
-        obj.assets.forEach(function (value, index, array) {
-            var newObj = {};
-            newObj.dateRetrieved = obj.dateRetrieved;
-            newObj.series = value.name;
-            newObj.download_count = value.download_count;
-            newInsert.push(newObj);
-        });
-        return newInsert;
-    });
+function graph (data) {
+    console.log(data);
 
-    var flattenedData = formattedData.reduce(function (a, b) {
-        return a.concat(b);
-    })
+    var width = 750;
+    var height = 500;
+    var padding = 50;
 
-    console.log(flattenedData);
+    // var x = d3.time.scale().domain().range();
+    // var y = d3.scale.linear().domain().range();
+
+
+
+
+
 }
 
+var url = 'https://www.openice.info/files/github-downloads-data.txt';
 
-var downloads = getDownloadsData(parseDownloads);
-
+getDownloadsData(url, function (data) {
+    graph(data);
+});

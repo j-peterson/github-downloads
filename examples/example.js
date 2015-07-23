185156076fbec6ba821091d343e709b21ddc75d7
j-peterson <jtpeterson@mdpnp.org>
2015-07-22 23:51:40 -0400

    more D3 graphing progress

--- examples/example.js
+++ examples/example.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var url = 'https://www.openice.info/files/github-downloads-data.txt';
+
 function getDownloadsData (url, callback) {
     function parseDownloads (data) {
         var formattedData = data.map(function (obj) {
@@ -37,20 +39,30 @@ function getDownloadsData (url, callback) {
 function graph (data) {
     console.log(data);
 
-    var width = 750;
-    var height = 500;
-    var padding = 50;
+    // function parseDate (date) {
+    //     return Date.parse(date);
+    // }
 
-    // var x = d3.time.scale().domain().range();
-    // var y = d3.scale.linear().domain().range();
+    var th = window.innerHeight * 0.75;
+    var tw = window.innerWidth * 0.75;
 
+    var padding = 75;
+
+    var h = th - padding * 2;
+    var w = tw - padding * 2;
+
+    var downloads = d3.select("#graph-container")
+        .append("svg:svg")
+        .attr("width", tw)
+        .attr("height", th);
+
+    // var x = d3.time.scale().domain().range([0, w]);
+    var y = d3.scale.linear().domain([0, d3.max(data, function(datum) { return datum.download_count; })]).range([0, h]);
 
 
 
 
 }
-
-var url = 'https://www.openice.info/files/github-downloads-data.txt';
 
 getDownloadsData(url, function (data) {
     graph(data);
